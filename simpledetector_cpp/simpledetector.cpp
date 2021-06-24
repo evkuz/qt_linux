f9059cf906446087c8e03ff521f15657063ff8b1
Mikhail Katulin <miksarus@jinr.ru>
2021-06-24 17:47:02 +0300

    added argparser and detector checking to simpledetector_cpp

--- simpledetector_cpp/simpledetector.cpp
+++ simpledetector_cpp/simpledetector.cpp
@@ -11,6 +11,8 @@
 #include <vector>
 
 #include "SocketServer.h"
+#include "arg_parser.h"
+
 
 using namespace cv;
 using namespace std;
@@ -19,6 +21,36 @@ typedef unsigned char byte;
 
 byte color_lower[3] = {80, 100, 190}; //{0, 20, 190};
 byte color_upper[3] = {180, 200, 255}; //{100, 140, 255};
+
+static int cameraId = 2;
+static int framesToCheck = 5;
+static const struct Miksarus::ProgramOption options_list[]{
+    {Miksarus::optVerbose, NOT_REQUIRED},
+    { 
+        {
+            "camera",          // long name
+            'c',                // short name 
+            required_argument,  // is option value required
+            "camera number (0 - any, default - 2)",
+            [](char* argv) { cameraId = atoi(argv); }, 
+    		nullptr
+        },
+		NOT_REQUIRED
+	},
+	{
+		{
+            "check-frames",          // long name
+            'f',                // short name 
+            required_argument,  // is option value required
+            "how many frames the object must be detected (default - 5)",
+            [](char* argv) { framesToCheck = atoi(argv); }, 
+    		nullptr
+        },
+        NOT_REQUIRED                 // is option required (true or false)
+    },
+    {Miksarus::optNULL, NOT_REQUIRED}
+};
+
 
 Rect detector(const Mat& frame, Mat& out, const byte* color_l, const byte* color_u){
 	Rect res = Rect(0, 0, 0, 0);
@@ -60,7 +92,7 @@ Rect detector(const Mat& frame, Mat& out, const byte* color_l, const byte* color
 
 int main(int argc, char* argv[])
 {
-        int cameraId = 2;
+    Miksarus::ParseProgramOptions(argc, argv, options_list);
 	
 	VideoCapture capture;
 	capture.open(cameraId);
@@ -68,10 +100,10 @@ int main(int argc, char* argv[])
     if(!capture.isOpened())
         return fprintf( stderr, "Could not initialize video (%d) capture\n", cameraId), -2;
 
-        if (!capture.set(CAP_PROP_FRAME_WIDTH, 1024))
-        {
-                std::cerr << "ERROR: seekeing is not supported" << endl;
-        }
+	if (!capture.set(CAP_PROP_FRAME_WIDTH, 1024))
+	{
+		std::cerr << "ERROR: seekeing is not supported" << endl;
+	}
 //CAP_PROP_FRAME_HEIGHT
 
 	Mat frame, image;
@@ -83,6 +115,10 @@ int main(int argc, char* argv[])
 		std::cerr << "Can't start socket server!" << endl;
 		return -3;
 	}
+
+	int framesSinceDetection = 0;
+	Rect etalon;
+	double etalon_border_coef = 1;
 	while(true) {
 		if(capture.isOpened())
 		{
@@ -91,14 +127,35 @@ int main(int argc, char* argv[])
 			view0.copyTo(frame);
 
 			Rect pos = detector(frame, image, color_lower, color_upper);
-			if(pos.width*pos.height > 0) {
+			int area = pos.width*pos.height;
+			if(area > 0) {
+				double border_coef = pos.width/pos.height;
+				int max_size = max(pos.width, pos.height);
+				int min_size = min(pos.width, pos.height);
 				int ox = static_cast<int>(pos.x + pos.width/2);
 				int oy = static_cast<int>(pos.y + pos.height/2);
-				printf("%i,%i\n", ox, oy);
-				rectangle(frame, pos, Scalar(0,255, 0));
-
-				server.SetDetectorState(true, ox, oy);
+				
+				if (border_coef > 0.7 && border_coef < 1.3 && max_size < 400 && min_size > 50){
+					printf("%i, %i (area: %i px^2)\n", ox, oy, area);
+					rectangle(frame, pos, Scalar(0,255, 0));
+					if( abs(pos.x - etalon.x) < 3 && abs(pos.y - etalon.y) < 3 ){
+						framesSinceDetection++;
+					} else {
+						etalon = pos;
+						framesSinceDetection = 1;
+					}
+				} else {
+					framesSinceDetection = 0;
+					server.SetDetectorState(false, 0, 0);
+				}
+				
+				if (framesSinceDetection >= framesToCheck){
+					framesSinceDetection = framesToCheck;
+					server.SetDetectorState(true, ox, oy);
+					printf("set_detected!\n");
+				}
 			} else {
+				framesSinceDetection = 0;
 				server.SetDetectorState(false, 0, 0);
 			}
 
