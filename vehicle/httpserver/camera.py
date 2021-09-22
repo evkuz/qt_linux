46476ea8314e4dbf9a9eef97fc3332a16765e26f
Mikhail Katulin <miksarus@jinr.ru>
2021-09-22 23:47:28 +0300

    video stream is working

--- vehicle/httpserver/camera.py
+++ vehicle/httpserver/camera.py
@@ -5,69 +5,92 @@ import threading
 
 class Camera(object):
     last_access = 0  # time of last client access to the camera
-    
+
     def __init__(self, camPort, width=None, height=None):
-            self.port = camPort
-            self.FrameWidth = width
-            self.FrameHeight = height
-            self.actualFrame = None
-            self._test_run()
-    
-    def __open(self, cap, nTries=3):
-        if cap.isOpened():
-            cap.release()
-        
+        self.port = camPort
+        self.FrameWidth = width
+        self.FrameHeight = height
+        self.__actualFrame = None
+        self.__thread = None
+        self.__cap = cv2.VideoCapture()
+        self.__isWorking = False
+        self.__waitForFrame = threading.Event()
+        self.__waitForFrame.clear()
+
+    @property
+    def isOpened(self):
+        return self.__cap.isOpened()
+
+    @property
+    def isStarted(self):
+        return self.__isWorking
+
+    def __open_device(self, nTries=3):
+        if self.isOpened:
+            self.__cap.release()
+
         for i in range(nTries):
-            cap.open(self.port)
-            if cap.isOpened():
+            self.__cap.open(self.port)
+            if self.__cap.isOpened():
                 break
         else:
             msg = f"Can't open camera port {self.port}!"
             print(msg)
             raise RuntimeError(msg)
-        
+
         if self.FrameWidth is not None:
-            cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.FrameWidth)
+            self.__cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.FrameWidth)
 
         if self.FrameHeight is not None:
-            cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.FrameHeight)
+            self.__cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.FrameHeight)
 
-    def _test_run(self):
-        cap = cv2.VideoCapture()
-        self.__open(cap)
-        frame = None
-        for i in range(5):
-            _, frame = cap.read()
-        cap.release()
-        self.actualFrame = frame
+    def __close_device(self):
+        self.__cap.release()
+
+    def start(self):
+        if self.__thread is not None:
+            return
+
+        self.__thread = threading.Thread(target=self.__thread_work)
+        self.__thread.start()
+
+    def stop(self):
+        if self.__thread is None:
+            return
+        self.__isWorking = False
+        self.__thread.join()
 
     def get_frame(self):
-        frame = self.actualFrame.copy()
+        if not self.isStarted:
+            self.start()
+        
+        self.__waitForFrame.wait()
+        self.__waitForFrame.clear()
+        frame = self.__actualFrame.copy()
         cv2.putText(
             frame,
             f"time={Camera.last_access}",
-			(100,20),
+         			(100, 20),
             cv2.FONT_HERSHEY_COMPLEX,
             0.5,
-            (0,0,0),
+            (0, 0, 0),
             1
         )
-        (flag, encodedImage) = cv2.imencode(".jpeg", frame)
         Camera.last_access = time.time()
-        return encodedImage
+        return frame
 
-    def start(self):
-        self.__class__.thread = threading.Thread(target=self.__class__.thread)
+    def __thread_work(self):
+        self.__isWorking = True
+        self.__open_device()
 
-    @classmethod
-    def _thread(cls):
-        # with cv2.VideoCapture() as cap:
-            
-        #     while True:
-        #         frame = cap.read()
-        #         (flag, encodedImage) = cv2.imencode(".jpg", frame)
+        while self.__isWorking:
+            _, frame = self.__cap.read()
+            self.__actualFrame = frame.copy()
+            self.__waitForFrame.set()
+            #the last 10 seconds stop the thread
+            if time.time() - self.last_access > 20:
+                break
 
-        #         # the last 10 seconds stop the thread
-        #         if time.time() - cls.last_access > 20:
-        #             break
-        cls.thread = None
+        self.__close_device()
+        self.__thread = None
+        self.__isWorking = False
--- vehicle/httpserver/streaming.py
+++ vehicle/httpserver/streaming.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, Response
 from camera import Camera
+from test_camera import TestCamera
+import cv2
+
 
 app = Flask(__name__)
 cam = Camera(0)
+#cam = TestCamera()
 
 
 @app.route('/')
@@ -11,9 +15,10 @@ def index():
 
 def gen(camera):
     while True:
-        frame = camera.get_frame().tobytes()
+        frame = camera.get_frame()
+        (flag, encodedImage) = cv2.imencode(".jpeg", frame)
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + encodedImage.tobytes() + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
