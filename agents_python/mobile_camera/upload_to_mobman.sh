65fa045feb7cd2ea78f6a2c1c65eda69c10e46f2
Mikhail Katulin <miksarus@jinr.ru>
2022-01-18 11:54:25 +0300

    added script to mobile_camera for uploading files to robot

---
+++ agents_python/mobile_camera/upload_to_mobman.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+cur_dir=${PWD##*/} 
+
+if [[ $cur_dir != "mobile_camera" ]]; then
+    echo "Please run it from 'mobile_camera' directory. Abort..."
+    exit 1
+fi
+
+cd ../../
+rsync -av --exclude '__pycache__' --exclude '.venv' agents_python pi@192.168.1.201:~/ 
