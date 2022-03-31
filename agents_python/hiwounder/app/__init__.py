8acbff08ef8c886a4c77f529afec74c692b3b153
Mikhail Katulin <miksarus@jinr.ru>
2022-03-31 11:09:57 +0300

    added hiwounder agent (not working)

---
+++ agents_python/hiwounder/app/__init__.py
@@ -0,0 +1,27 @@
+import sys
+import pathlib
+module_path = pathlib.Path(__file__).parent.parent.parent.resolve()
+sys.path.append(str(module_path))
+
+import os
+from iqrdevice import device, app
+
+# создание экземпляра приложения
+app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
+
+from . import utils
+cam = utils.CameraDetector(app.config['CAMERA_NUM'])
+
+# import views
+from . import views
+
+# import robot features
+from . import actions
+from . import services
+
+device.set_name("mobile camera")
+getPositionService = services.CamDetectorService(cam)
+device.register_service(services.CamCalibService(cam))
+
+device.register_service(getPositionService)
+device.register_action(actions.MoveToTakeCube("http://192.168.1.205:8082", getPositionService, "http://192.168.1.201:8383"))
---
+++ agents_python/hiwounder/app/actions/__init__.py
@@ -0,0 +1,5 @@
+from .movetotakecube import MoveToTakeCube
+
+__all__ = [
+        'MoveToTakeCube',
+        ]
