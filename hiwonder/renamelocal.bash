605d7222b5e0342e89fe94e21e8caa7bc0bd86cd
Evgeniy Kuznetsov <evkuz@inbox.ru>
2022-05-31 15:25:47 +0300

    31.05.2022 1st After fixed merging problem

---
+++ hiwonder/renamelocal.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#cd ${EXTLIBS}/manipulator
+cd hiwonder-web
+
+mv qsimpleserver.cpp qsimpleserver.cpp00
+mv qsimpleserver.h qsimpleserver.h00
+mv qsocketthread.cpp qsocketthread.cpp00
+mv qsocketthread.h qsocketthread.h00
+mv SocketClient.cpp SocketClient.cpp00
+mv SocketClient.h SocketClient.h00
+
+mv mcinfer.cpp mcinfer.cpp00
+mv mcinfer.h mcinfer.h00
