e648b8cc119e26ba18c3144641eb82b3d5b180b3
Evgeniy Kuznetsov <evkuz@inbox.ru>
2022-08-24 09:05:08 +0300

    24.08.2022 qtlibs added

---
+++ qtlibs/manipulator/meta/qsimpleserver.h
@@ -0,0 +1,70 @@
+/*
+ *
+ * Класс QSimpleServer - для объекта сервер QTcpServer
+ *
+*/
+#ifndef QSIMPLESERVER_H
+#define QSIMPLESERVER_H
+
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QDebug>
+#include <QDateTime>
+#include <QTime>
+#include <QObject>
+#include <QString>
+#include "qsocketthread.h"
+#include <QMetaObject>
+
+//#define tcpport 8383;  // Порт, который слушает QTcpServer
+
+class QSimpleServer : public QTcpServer
+{
+    Q_OBJECT
+public:
+    //Конструктор
+    //explicit QSimpleServer(const QMetaObject* ptrMainProcess,  QObject *parent = nullptr);
+    explicit QSimpleServer(QObject* ptrMainProcess,  QObject *parent = nullptr);
+
+    // http://localhost:8383/run?cmd=status&
+    // http://192.168.1.175:8383/run?cmd=status&
+#define  wifi_ADDRESS "192.168.1.175"
+#define  ipv4_ADDRESS "159.93.84.44"
+
+    QThread   *thread_A;
+    QSocketThread *tcpthread;
+    quint16 tcpport = 8383;  // Порт, который слушает QTcpServer
+    QByteArray tcpdata;
+    bool isCreatedSocket; // socket creation flag
+    //QTcpSocket *createdSocket;
+    int counterConnections; // counter for incoming tcp connections
+    //Метод, вызываемый при подключении нового соединения.
+    void incomingConnection(qintptr sDescriptor) override; // virtual YES !!! override EXACTLY !
+    volatile int  clientsCounter; // counter for sockets being processed
+signals:
+    // Сигнал на отправку данных наверх, уже роботу.
+    void Data_From_TcpClient_Signal(QString, qintptr socketNumber);
+    // Сигнал на отправку данных вниз, в сокет
+    void Data_2_TcpClient_Signal(QString, qintptr socketNumber);
+
+
+public slots:
+    // Слот принятия строки для отправки клиенту в сокет
+    // Обработчик сигнала сверху  Write_2_Client_Signal(QString)
+    // void Write_2_TcpClient_SLot(QString, qintptr socketNumber);
+    //Слот отправки наверх команды на выполнение
+    // Срабатывает по сигналу ommand_4_Parsing_Signal(QString);
+    //void Command_4_Parsing_Slot(QString, qintptr);
+
+    //Слот получает указатель на вновь созданный сокет - обект QTcpSocket*
+    void isCreatedSocket_Slot(QTcpSocket*); // Слот сигнала QSocketThread::isCreatedSocket_Signal(QTcpSocket);
+    void finshQThread_Slot();
+    void makePause_Slot();
+    void resumePause_Slot();
+    void decSocketCounter_Slot();
+
+private:
+    QObject* ptrServerToMainProc;
+};
+
+#endif // QSIMPLESERVER_H
