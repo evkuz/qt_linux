abd447eb3775fadb467d20131479636be173052a
Evgeniy Kuznetsov <evkuz@inbox.ru>
2022-10-28 13:25:50 +0300

    mobPlatform class added

---
+++ arduino_mega/mobPlatform/Pololu_VNH5019_EK/mobPlatform.ino
@@ -0,0 +1,21 @@
+class mobPlatform{
+  
+  public:
+    void mobPlatform ();
+  
+  };
+
+// Initiate mpbPlatform parameters
+  void mobPlatform ()
+  {
+    int diameter = 90; // 90mm
+    int m1A_count, m1B-Count, m2A_count, m2B_count;
+    
+    // set the value of rotation counts
+    m1A_count = 486; // 
+    m1B-Count = 487;
+
+    m2A_count = 473;
+    m2B_count = 473; 
+    
+    }
